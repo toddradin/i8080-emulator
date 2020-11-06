cf0940d97374e2020bd4795c277ac841ec933598
toddradin <todd.radin@gmail.com>
2020-11-05 18:26:20 -0600

    Removed test code

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use cpu::Cpu;
 use instruction::Instruction;
 use std::fs::File;
 use std::io::Read;
-use std::process;
 
 fn load_roms(buffer: &mut [u8]) -> std::io::Result<()> {
     let mut addr = 0x00;
@@ -21,41 +20,23 @@ fn load_roms(buffer: &mut [u8]) -> std::io::Result<()> {
 
 fn main() -> Result<(), std::io::Error> {
     let mut cpu = Cpu::new();
-    //match load_test_rom(&mut cpu.memory) {
-    //    Ok(_) => (),
-    //    Err(error) => panic!("Problem opening the file: {:?}", error),
-    //}
+    match load_roms(&mut cpu.memory) {
+        Ok(_) => (),
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    }
 
-    let offset = 0x100;
-    let buffer = include_bytes!("../roms/cpudiag.bin");
-    cpu.memory[offset as usize..(buffer.len() + offset as usize)].copy_from_slice(buffer);
-    cpu.pc = 0x100;
-
-    cpu.memory[368] = 0x7;
     let debug = false;
-
-    let mut i = 0;
     while cpu.pc < cpu.memory.len() as u16 {
         let instr = Instruction::from(&cpu.memory[cpu.pc as usize..]);
-        println!("{:?} {:?}", i, instr);
         let (next_pc, cycles) = cpu.execute(&instr);
         cpu.pc = next_pc;
 
         if debug {
+            println!("{:?}", instr);
             println! {"pc: {:#x?}, sp: {:#x?},", cpu.pc, cpu.sp};
             println!("cycles: {}", cycles);
             println!("{:#x?}", cpu.condition_codes);
             println!("{:#x?}\n", cpu.registers);
-        }
-        i += 1;
-
-        if cpu.pc == 0x689 {
-            panic!("Encountered error at instruction {:#x?}", instr);
-        }
-
-        if cpu.pc == 0x69B {
-            println!("Successfully ran test rom");
-            process::exit(0)
         }
     }
 
